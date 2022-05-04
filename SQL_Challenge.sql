ac8fd7d2e171a9282ee9cfb523f8aee7e0b3f708
Chris Chung <78888379+cchung106@users.noreply.github.com>
2022-05-04 09:22:25 -0400

    Update SQL_Challenge.sql

--- SQL_Challenge.sql
+++ SQL_Challenge.sql
@@ -1,20 +1,10 @@
 /* Welcome to the SQL mini project. You will carry out this project partly in
 the PHPMyAdmin interface, and partly in Jupyter via a Python connection.
 
-This is Tier 2 of the case study, which means that there'll be less guidance for you about how to setup
-your local SQLite connection in PART 2 of the case study. This will make the case study more challenging for you: 
-you might need to do some digging, aand revise the Working with Relational Databases in Python chapter in the previous resource.
-
-Otherwise, the questions in the case study are exactly the same as with Tier 1. 
-
 PART 1: PHPMyAdmin
 You will complete questions 1-9 below in the PHPMyAdmin interface. 
 Log in by pasting the following URL into your browser, and
 using the following Username and Password:
-
-URL: https://sql.springboard.com/
-Username: student
-Password: learn_sql@springboard
 
 The data you need is in the "country_club" database. This database
 contains 3 tables:
